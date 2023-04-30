27ca1e1f823edbdd12973487f13b61809caf2f84
Touhidur Zaman <touhidcodes@gmail.com>
2023-04-30 22:38:10 +0600

    marquee

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import logo from "../../../../src/assets/img/logo.png";
 import moment from "moment";
+import { Button } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
 	return (
-		<div className='text-center'>
-			<img src={logo} alt='' className='mt-5' />
-			<h5 className='mt-3 text-secondary'>Journalism Without Fear or Favour</h5>
-			<h5 className='mt-3 '>{moment().format("LLLL")}</h5>
+		<div>
+			<div className='text-center'>
+				<img src={logo} alt='' className='mt-5' />
+				<h5 className='mt-3 text-secondary'>
+					Journalism Without Fear or Favour
+				</h5>
+				<h5 className='mt-3 '>{moment().format("LLLL")}</h5>
+			</div>
+			<div className='d-flex bg-light p-3 rounded '>
+				<Button variant='danger'>Latest</Button>
+				<Marquee speed={80} className='text-danger'>
+					<h5>
+						Match Highlights: Germany vs Spain — as it happened ! Match
+						Highlights: Germany vs Spain as...
+					</h5>
+				</Marquee>
+			</div>
 		</div>
 	);
 };
