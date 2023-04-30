e3ce95ba7a8d478520fcfac45616061af1f6268d
Touhidur Zaman <touhidcodes@gmail.com>
2023-04-30 19:38:04 +0600

    router-setup

---
+++ src/Layouts/Root.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "../Pages/Home/Home/Home";
+import Footer from "../Pages/Shared/Footer/Footer";
+
+const Root = () => {
+	return (
+		<div>
+			<Home />
+			<Footer />
+		</div>
+	);
+};
+
+export default Root;
