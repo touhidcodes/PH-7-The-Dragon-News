de61624c711bacfafa55cbbf888e4cffb2ed4266
Touhidur Zaman <touhidcodes@gmail.com>
2023-04-30 22:23:32 +0600

    moment-js

--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import Home from "../Pages/Home/Home/Home";
 import Footer from "../Pages/Shared/Footer/Footer";
 import { Col, Container, Row } from "react-bootstrap";
 import LeftNav from "../Pages/Shared/leftNav/leftNav";
 import RightNav from "../Pages/Shared/RightNav/RightNav";
 import "./Root.css";
+import Header from "../Pages/Shared/Header/Header";
 
 const Root = () => {
 	return (
 		<div className='layout'>
-			<Home />
+			<Header />
 			<Container>
 				<Row>
 					<Col lg={3}>
--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import logo from "../../../../src/assets/img/logo.png";
+import moment from "moment";
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<div className='text-center'>
+			<img src={logo} alt='' className='mt-5' />
+			<h5 className='mt-3 text-secondary'>Journalism Without Fear or Favour</h5>
+			<h5 className='mt-3 '>{moment().format("LLLL")}</h5>
+		</div>
+	);
 };
 
 export default Header;
