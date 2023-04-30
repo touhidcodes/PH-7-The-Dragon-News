b39b45e71fb8abb57276e0cdbbcf388971a5702a
Touhidur Zaman <touhidcodes@gmail.com>
2023-04-30 22:49:16 +0600

    responsive-navbar

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../../../../src/assets/img/logo.png";
 import moment from "moment";
-import { Button } from "react-bootstrap";
+import { Button, Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 import Marquee from "react-fast-marquee";
 
 const Header = () => {
@@ -22,6 +22,24 @@ const Header = () => {
 						Highlights: Germany vs Spain as...
 					</h5>
 				</Marquee>
+			</div>
+			<div>
+				<Navbar collapseOnSelect expand='lg' bg='light' variant='light'>
+					<Container>
+						<Navbar.Toggle aria-controls='responsive-navbar-nav' />
+						<Navbar.Collapse id='responsive-navbar-nav'>
+							<Nav className='mx-auto'>
+								<Nav.Link href='#features'>Home</Nav.Link>
+								<Nav.Link href='#pricing'>About</Nav.Link>
+								<Nav.Link href='#pricing'>Career</Nav.Link>
+							</Nav>
+							<Nav>
+								<Nav.Link href='#deets'>Profile</Nav.Link>
+								<Button variant='secondary'>Log In</Button>
+							</Nav>
+						</Navbar.Collapse>
+					</Container>
+				</Navbar>
 			</div>
 		</div>
 	);
